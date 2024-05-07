3e58bd7bd63ecfaf52cf09628b05b780bb1a5392
Daniela Carballo <daniela.carballo@biggertech.co>
2024-05-07 01:24:57 -0300

    Add placeholderData to useQuery

--- 02-react-query-issues/src/issues/hooks/useLabels.tsx
+++ 02-react-query-issues/src/issues/hooks/useLabels.tsx
@@ -13,6 +13,24 @@ const getLabels = async (): Promise<Label[]> => {
 export const useLabels = () => {
   const labelsQuery = useQuery(["labels"], getLabels, {
     staleTime: 1000 * 60 * 60,
+    placeholderData: [
+      {
+        id: 717031390,
+        node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+        name: "good first issue",
+        color: "6ce26a",
+        default: true,
+      },
+      {
+        id: 6344006318,
+        node_id: "LA_kwDOAJy2Ks8AAAABeiHarg",
+        url: "https://api.github.com/repos/facebook/react/labels/fb-exported",
+        name: "fb-exported",
+        color: "ededed",
+        default: false,
+      },
+    ],
   });
 
   return labelsQuery;
