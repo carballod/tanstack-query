6ff7d63ada4225296f1b18ffff98021b53ca6a47
Daniela Carballo <daniela.carballo@biggertech.co>
2024-05-07 00:35:05 -0300

    Add label data type

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,14 +1,28 @@
+import { useQuery } from "react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>("/labels");
+
+  console.log(data);
+
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery(["labels"], getLabels, {
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
