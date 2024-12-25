ee8837eb2f5cada699fa9740c9d2ae1995687db8
Daniela Carballo <daniela.carballo@biggertech.co>
2024-12-25 13:58:27 -0300

    Add use labels custom hook using react query

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,3 +1,6 @@
+import { LoadingSpinner } from "../../shared";
+import { useLabels } from "../hooks/useLabels";
+
 export const LabelPicker = () => {
   const { labelsQuery } = useLabels();
 
@@ -10,13 +13,18 @@ export const LabelPicker = () => {
   }
 
   return (
-    <>
-      <span
-        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-      >
-        Primary
-      </span>
-    </>
+    <div className="flex flex-wrap gap-2 justify-center">
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className="animate-fade-in px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+          style={{
+            border: `1px solid #${label.color}`,
+          }}
+        >
+          {label.name}
+        </span>
+      ))}
+    </div>
   );
 };
